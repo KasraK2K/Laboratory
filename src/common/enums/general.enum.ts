c01a74cbe2295b25ddd5703269f59ab5db8c245e
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-20 15:40:27 +0330

    write general enums

---
+++ src/common/enums/general.enum.ts
@@ -0,0 +1,20 @@
+export enum RoleName {
+  USER,
+  RECEPTIONIST,
+  DOCTOR
+}
+
+export enum PermissionName {
+  NORMAL,
+  VIP
+}
+
+export enum TokenType {
+  TOKEN,
+  REFRESF_TOKEN
+}
+
+export enum StrategyName {
+  PORTAL_USER = 'portal_user',
+  APP_USER = 'app_user'
+}
