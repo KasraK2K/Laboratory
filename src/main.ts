463e1d477a1370e6d1d0a028199068e1f5c2f3da
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-20 15:41:04 +0330

    write general interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger(bootstrap.name)
+
+  const port = process.env.PORT || 3000
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+  app.enableVersioning({
+    defaultVersion: process.env.DEFAULT_API_VERSION,
+    type: VersioningType.URI
+  })
+
+  const config = new DocumentBuilder()
+    .setTitle('Other')
+    .setDescription('Laboratory Service')
+    .setVersion('1.0.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger', app, document)
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true })) // Force to use validation in each API
+
+  await app
+    .listen(port, '0.0.0.0')
+    .then(() => logger.log(`Laboratory service version ${process.env.DEFAULT_API_VERSION} is running on port ${port}`))
+    .catch((error) => logger.error(error))
 }
-bootstrap();
+bootstrap()
