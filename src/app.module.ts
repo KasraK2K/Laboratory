2784c9ccd14ae686a71485b19da5af43ef4b4c20
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-20 15:39:54 +0330

    init app and install needed dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { DataSource } from 'typeorm'
+import { ConfigModule } from '@nestjs/config'
+import { MysqlDatabaseType } from './common/interfaces/general.interface'
+import { AppUserModule } from './app-user/app-user.module'
+import { AuthModule } from './auth/auth.module'
+import { PortalUserModule } from './portal-user/portal-user.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'development'
+          ? ['.env.development', '.env']
+          : process.env.NODE_ENV === 'production'
+          ? ['.env.production', '.env']
+          : ['.env.local', '.env'],
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as MysqlDatabaseType,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV === 'development',
+      autoLoadEntities: true
+    }),
+    AppUserModule,
+    AuthModule,
+    PortalUserModule
+  ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
